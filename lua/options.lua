6e79d7cca66e7887bfe6a65fcb96ea527c2512a4
TeomanM <teomanmdr@protonmail.com>
2025-02-21 21:49:12 +0300

    New plugins etc

--- lua/options.lua
+++ lua/options.lua
@@ -2,7 +2,12 @@ require "nvchad.options"
 vim.filetype.add {
   pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
 }
--- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- No line wraps.
+local o = vim.o
+o.wrap = false
+
+-- Relative line number 
+o.number = true
+o.relativenumber = true
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,7 +23,31 @@ return {
         "html",
         "css",
         "hyprlang",
+        "glsl",
       },
     },
+  },
+
+  {
+    "mrjones2014/smart-splits.nvim",
+  },
+  {
+    "maxmx03/fluoromachine.nvim",
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --   local fm = require "fluoromachine"
+    --
+    --   fm.setup {
+    --     glow = true,
+    --     theme = "fluoromachine",
+    --     transparent = true,
+    --   }
+    --
+    --   vim.cmd.colorscheme "fluoromachine"
+    -- end,
+  },
+  {
+    "vuki656/package-info.nvim",
   },
 }
