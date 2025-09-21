fdad769a383a1822f07410be69f7cda3b8aa4c18
TeomanM <49800935+TeomanM@users.noreply.github.com>
2025-09-21 17:38:57 +0300

    Include docker-compose lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "rust_analyzer", "ts_ls", "glsl_analyzer", "clangd", "ruff" }
+local servers = { "html", "cssls", "rust_analyzer", "ts_ls", "glsl_analyzer", "clangd", "ruff", "docker_compose_language_service" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
