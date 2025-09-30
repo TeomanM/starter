203049a67a3990a4c6d5fbf2e5f54085f3a01696
TeomanM <49800935+TeomanM@users.noreply.github.com>
2025-10-01 00:44:45 +0300

    systemd

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "rust_analyzer", "ts_ls", "glsl_analyzer", "clangd", "ruff", "docker_compose_language_service" }
+local servers = { "html", "cssls", "rust_analyzer", "ts_ls", "glsl_analyzer", "clangd", "ruff", "docker_compose_language_service", "systemd-language-server", "systemdlint" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
