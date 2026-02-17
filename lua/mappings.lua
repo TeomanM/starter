6b3a5e8499e60dd8979200700b1fc0634d5cef9b
TeomanM <49800935+TeomanM@users.noreply.github.com>
2026-02-17 18:46:58 +0300

    Refactor Telescope mappings for improved documentation search and add new telescope configuration file

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,12 +8,10 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map(
 	"n",
 	"<leader>fs",
-	"<cmd>Telescope find_files search_dirs={'/usr/share/doc/arch-wiki/html/en/'}<cr>",
-	{ desc = "Telescope Search Arch Wiki" }
+	"<cmd>Telescope find_files search_dirs={'/usr/share/doc'}<cr>",
+	{ desc = "Telescope Search Docs" }
 )
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+map("n", "<leader>mp", "<cmd>Telescope man_pages sections={'ALL'}<cr>", { desc = "Telescope Search Manpages" })
 
 -- Disable mappings
 -- local nomap = vim.keymap.del
